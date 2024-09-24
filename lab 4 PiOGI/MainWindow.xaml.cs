d9b2d06f651745c266fd2f33e46a586fea226ad9
KittyMur <kotyanya7000@mail.ru>
2024-09-24 09:47:17 +0700

    first commit

---
+++ lab 4 PiOGI/MainWindow.xaml.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab_4_PiOGI
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+        BitmapImage mine = new BitmapImage(new Uri(@"pack://application:,,,/imgs/Minee.png", UriKind.Absolute));
+
+        int N = 10;
+        int K = 5;
+        int J = 15;
+        int size = 30;
+        int wincheck = 0;
+       
+        cGen gen;
+     
+
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+           
+        
+            Image img = new Image();
+            img.Source = mine;
+            StackPanel stackPnl = new StackPanel();
+            stackPnl.Margin = new Thickness(1);
+            stackPnl.Children.Add(img);
+            ((Button)sender).Content = stackPnl;
+
+            int i = (int)((Button)sender).Tag;
+            ((Button)sender).Background = Brushes.White;
+            ((Button)sender).Foreground = Brushes.Pink;
+            ((Button)sender).FontSize = 18;
+
+            if (gen.getCell(i % N, i / N) == 9)
+            {
+                img.Source = mine;
+                MessageBox.Show("MEOW!!! YOU DIED!!! MEOW!!! TRY AGAIN!!!");
+                ugr.Children.Clear();
+                wincheck = 0;
+            }
+            else
+            {
+                wincheck = wincheck + 1;
+                ((Button)sender).Content = gen.getCell(i % N, i / N);
+                
+            }
+            if (wincheck == 84)
+            {
+                MessageBox.Show("MEOW!!! YOU WIN!!! MEOW!!!");
+                ugr.Children.Clear();
+                wincheck = 0;
+            }
+
+
+        }
+
+
+        private void Btn1_Click(object sender, RoutedEventArgs e)
+        {
+
+            Image img = new Image();
+            img.Source = mine;
+            StackPanel stackPnl = new StackPanel();
+            stackPnl.Margin = new Thickness(1);
+            stackPnl.Children.Add(img);
+            ((Button)sender).Content = stackPnl;
+
+            int i = (int)((Button)sender).Tag;
+            ((Button)sender).Background = Brushes.White;
+            ((Button)sender).Foreground = Brushes.Pink;
+            ((Button)sender).FontSize = 18;
+
+            if (gen.getCell(i % K, i / K) == 9)
+            {
+                img.Source = mine;
+                MessageBox.Show("MEOW!!! YOU DIED!!! MEOW!!!");
+                ugr.Children.Clear();
+                wincheck = 0;
+            }
+            else
+            {
+                wincheck = wincheck + 1;
+                ((Button)sender).Content = gen.getCell(i % K, i / K);
+               
+            }
+            if (wincheck == 21)
+            {
+                MessageBox.Show("MEOW!!! YOU WIN!!! MEOW!!!");
+                ugr.Children.Clear();
+                wincheck = 0;
+            }
+
+
+
+        }
+
+        private void Large_Click(object sender, RoutedEventArgs e)
+        {
+
+            gen = new cGen(J);
+            gen.generate();
+
+
+            ugr.Rows = J;
+            ugr.Columns = J;
+
+            ugr.Width = J * (size + 4);
+            ugr.Height = J * (size + 4);
+
+            ugr.Margin = new Thickness(5, 5, 5, 5);
+
+            ugr.Children.Clear();
+
+            for (int i = 0; i < J * J; i++)
+            {
+                Button btn = new Button();
+
+                btn.Tag = i;
+                btn.Width = size;
+                btn.Height = size;
+
+                btn.Margin = new Thickness(2);
+
+                btn.Click += Btn2_Click;
+
+                ugr.Children.Add(btn);
+            }
+        }
+
+            private void Btn2_Click(object sender, RoutedEventArgs e)
+            {
+                Image img = new Image();
+                img.Source = mine;
+                StackPanel stackPnl = new StackPanel();
+                stackPnl.Margin = new Thickness(1);
+                stackPnl.Children.Add(img);
+                ((Button)sender).Content = stackPnl;
+
+                int i = (int)((Button)sender).Tag;
+                ((Button)sender).Background = Brushes.White;
+                ((Button)sender).Foreground = Brushes.Pink;
+                ((Button)sender).FontSize = 18;
+
+                if (gen.getCell(i % J, i / J) == 9)
+                {
+                    img.Source = mine;
+                    MessageBox.Show("MEOW!!! YOU DIED!!! MEOW!!!");
+                    ugr.Children.Clear();
+                    wincheck = 0;
+            }
+            else
+            {
+                wincheck = wincheck + 1;
+                ((Button)sender).Content = gen.getCell(i % J, i / J);
+            }
+            if (wincheck == 188)
+            {
+                MessageBox.Show("MEOW!!! YOU WIN!!! MEOW!!!");
+                ugr.Children.Clear();
+                wincheck = 0;
+            }
+            
+    }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            gen = new cGen(K);
+            gen.generate();
+
+            ugr.Rows = K;
+            ugr.Columns = K;
+
+            ugr.Width = K * (size + 4);
+            ugr.Height = K * (size + 4);
+
+            ugr.Margin = new Thickness(5, 5, 5, 5);
+
+            ugr.Children.Clear();
+
+            for (int i = 0; i < K * K; i++)
+            {
+                Button btn = new Button();
+
+                btn.Tag = i;
+                btn.Width = size;
+                btn.Height = size;
+
+                btn.Margin = new Thickness(2);
+
+                btn.Click += Btn1_Click;
+
+                ugr.Children.Add(btn);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            gen = new cGen(N);
+            gen.generate();
+
+
+            ugr.Rows = N;
+            ugr.Columns = N;
+
+            ugr.Width = N * (size + 4);
+            ugr.Height = N * (size + 4);
+
+            ugr.Margin = new Thickness(5, 5, 5, 5);
+
+            ugr.Children.Clear();
+
+            for (int i = 0; i < N * N; i++)
+            {
+                Button btn = new Button();
+
+                btn.Tag = i;
+                btn.Width = size;
+                btn.Height = size;
+
+                btn.Margin = new Thickness(2);
+
+                btn.Click += Btn_Click;
+
+                ugr.Children.Add(btn);
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            gen = new cGen(J);
+            gen.generate();
+
+
+            ugr.Rows = J;
+            ugr.Columns = J;
+
+            ugr.Width = J * (size + 4);
+            ugr.Height = J * (size + 4);
+
+            ugr.Margin = new Thickness(5, 5, 5, 5);
+
+            ugr.Children.Clear();
+
+            for (int i = 0; i < J * J; i++)
+            {
+                Button btn = new Button();
+
+                btn.Tag = i;
+                btn.Width = size;
+                btn.Height = size;
+
+                btn.Margin = new Thickness(2);
+
+                btn.Click += Btn2_Click;
+
+                ugr.Children.Add(btn);
+            }
+        }
+    }
+    }
+
+
+    
+
---
+++ lab 4 PiOGI/cGen.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_4_PiOGI
+{
+    //0 - empty
+    //1-8 - mines around
+    //9 - mine
+    public class cGen
+    {
+        int[,] field; 
+        int minCount;
+
+        Random rng = new Random();
+        public cGen(int N)
+      {
+            field = new int[N, N];
+            minCount = (N * N) / 6;
+      }
+
+        bool chekEmpty()
+        {
+            bool res = false;
+
+            for (int i = 0; i < field.GetLength(0); i++)
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+                    if (field[i, j] == 9)
+                    {
+                        res = false; 
+
+                        int l = i - 1;
+                        if (l < 0) l = 0;
+                        int r = i + 1;
+                        if (r >= field.GetLength(0)) r = field.GetLength(0) - 1;
+
+                        int u = j - 1;
+                        if (u < 0) u = 0;
+                        int d = j + 1;
+                        if (d >= field.GetLength(1)) d = field.GetLength(1) - 1;
+
+                        for (int i1 = l; i1 <= r; i1++)
+                            for (int j1 = u; j1 <= d; j1++)
+                            {
+                               if (field[i1, j1] == 0)
+                                {
+                                    res = true;
+                                    break;
+                                }
+                            }
+                        if (res == false)
+                            return false;
+                    }
+                }
+            return true;
+        }
+
+        void plantMines()
+        {
+            for (int i = 0; i < minCount; )
+            {
+                int x = rng.Next(0, field.GetLength(0));
+                int y = rng.Next(0, field.GetLength(1));
+
+                if (field[x, y] == 9) continue;
+
+                field[x, y] = 9;
+
+                if (chekEmpty() == false)
+                {
+                    field[x, y] = 0;
+                    continue;
+                }
+                i++;
+            }
+        }
+        void calculateCells()
+        {
+            for (int i = 0; i < field.GetLength(0); i++)
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+                   if  (field[i, j] == 0)
+                    {
+                        int l = i - 1;
+                        if (l < 0) l = 0;
+                        int r = i + 1;
+                        if (r >= field.GetLength(0)) r = field.GetLength(0) - 1;
+
+                        int u = j - 1;
+                        if (u < 0) u = 0;
+                        int d = j + 1;
+                        if (d >= field.GetLength(1)) d = field.GetLength(1) - 1;
+
+                        int sum = 0;
+
+                        for (int i1 = l; i1 <= r; i1++)
+                            for (int j1 = u; j1 <= d; j1++)
+                            {
+                                if (field[i1, j1] == 9) sum++;
+                            }
+                        field[i, j] = sum;
+                    }
+                }
+        }
+
+        public void generate()
+        {
+            plantMines();
+            calculateCells();
+        }
+        public int getCell(int i, int j)
+        {
+            return field[i, j];
+        }
+    }
+}
+  
